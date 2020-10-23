7c99a2efa1bf5acd3ea3e4734d3617d2f5701640
Discode Stu <dstuart85@yahoo.com>
2020-10-23 17:21:16 -0400

    added react query devtools

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 function App() {
   const [page, setPage] = useState("planets");
 
   return (
+    <>
     <div className="App">
       <h1>Star Wars Info</h1>
       <Navbar setPage={setPage} />
@@ -14,6 +16,8 @@ function App() {
         {page === "planets" ? <Planets /> : <People />}
       </div>
     </div>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
